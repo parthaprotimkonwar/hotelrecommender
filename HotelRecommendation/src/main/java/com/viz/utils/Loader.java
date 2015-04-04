6e7d91bdfd4e0fbd6c9a44f7ce3b9f8e0984f605
parthaprotimkonwar <parthaprotimkonwar@gmail.com>
2015-04-05 04:10:41 +0530

    up

--- HotelRecommendation/src/main/java/com/viz/utils/Loader.java
+++ HotelRecommendation/src/main/java/com/viz/utils/Loader.java
@@ -2,6 +2,7 @@ package com.viz.utils;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Map.Entry;
 
@@ -84,10 +85,12 @@ public class Loader {
 		//File hotelFile = new File(Constants.path + "finaldata.txt");
 
 		//File hotelFile = Loader.class.getClassLoader().getResourceAsStream("tags.txt");
-
-		try (FileInputStream hotelfi = (FileInputStream) Loader.class.getClassLoader().getResourceAsStream("finaldata.txt");
+		
+		try /*(FileInputStream hotelfi = (FileInputStream) Loader.class.getClassLoader().getResourceAsStream("finaldata.txt");
 				BufferedReader br = new BufferedReader(new InputStreamReader(
-						hotelfi))) {
+						hotelfi))) */{
+			InputStream io = Loader.class.getClassLoader().getResourceAsStream("finaldata.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(io));
 			String line = "";
 			while ((line = br.readLine()) != null) {
 				line = line.replaceAll("\\s", "");
