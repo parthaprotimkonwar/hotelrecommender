28c053a6dd11b7134f81e4b1c84c343ebadc853a
parthaprotimkonwar <parthaprotimkonwar@gmail.com>
2015-04-04 12:58:47 +0530

    upadated with a filter

---
+++ HotelRecommendation/src/main/java/testing/CorsFilter.java
@@ -0,0 +1,56 @@
+package testing;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class CorsFilter
+ */
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CorsFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		System.out.println("FILTER");
+		HttpServletResponse resp = (HttpServletResponse) response;
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		resp.setHeader("Access-Control-Max-Age", "3600");
+		resp.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		chain.doFilter(request, resp);
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
