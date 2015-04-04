579401250d8b8960f6ae00515f4c709a1c7be672
parthaprotimkonwar <parthaprotimkonwar@gmail.com>
2015-04-04 12:44:34 +0530

    updated

--- HotelRecommendation/src/main/java/testing/HelloWorldServlet.java
+++ HotelRecommendation/src/main/java/testing/HelloWorldServlet.java
@@ -25,6 +25,7 @@ public class HelloWorldServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("GET Method Called");
 		response.getWriter().println("Welcome welcome");
 	}
 
